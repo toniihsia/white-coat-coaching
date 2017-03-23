6269e96e3a06b1941aa7227b3dee1a8d7afdb611
toniihsia <tonia.hsia@gmail.com>
2017-03-23 10:52:52 -0700

    attempt at toggling info on title click (sunsama test)

--- frontend/components/residencies/residency_item.jsx
+++ frontend/components/residencies/residency_item.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link, hashHistory } from 'react-router';
+import ResidencyItemInfo from './residency_item_info';
 
 class ResidencyItem extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = { showInfo: false };
+
     this._handleClick = this._handleClick.bind(this);
+    this._showInfo = this._showInfo.bind(this);
   }
 
   _handleClick(e){
@@ -13,17 +17,23 @@ class ResidencyItem extends React.Component {
     this.props.handleClick(this.props.residency);
   }
 
+  _showInfo(e) {
+    e.preventDefault();
+    this.setState({showInfo: !this.state.showInfo});
+  }
+
   render() {
     let residency = this.props.residency;
 
     return (
-      <li className={`residency-item ${this.props.selected ? 'selected-item' : ''}`} onClick={this._handleClick}>
+      <li className={`residency-item ${this.props.selected ? 'selected-item' : ''}`} onClick={this._showInfo}>
         <div className='res-container'>
           <img className='res-photo' src={residency.image_url} alt={`${residency.name}${residency.id}`} />
           <div className='res-info'>
             <div className='res-title'>{residency.name}</div>
-            <div className='res-url'>{residency.website_url}</div>
-            <div className='res-description'>{residency.description}</div>
+            {this.state.showInfo &&
+              <ResidencyItemInfo
+                websiteUrl={residency.website_url} description={residency.description} />}
           </div>
         </div>
       </li>
@@ -32,3 +42,6 @@ class ResidencyItem extends React.Component {
 }
 
 export default ResidencyItem;
+
+// <div className='res-url'>{residency.website_url}</div>
+// <div className='res-description'>{residency.description}</div>
---
+++ frontend/components/residencies/residency_item_info.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link, hashHistory } from 'react-router';
+
+class ResidencyItemInfo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='res-url'>{this.props.website_url}</div>
+        <div className='res-description'>{this.props.description}</div>
+      </div>
+    );
+  }
+}
+
+export default ResidencyItemInfo;
